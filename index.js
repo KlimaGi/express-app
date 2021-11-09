e93caf9265c99a5c2f639490d3f9fcac0c0ef317
Giedre <giedre.klima@gmail.com>
2021-11-09 23:02:27 +0200

    SECURING RESTful APIs base project

--- index.js
+++ index.js
@@ -1,75 +1,30 @@
 import express from "express";
-
-import data from "./data/data.json";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import routes from "./src/routes/crmRoutes";
 
 const app = express();
 const PORT = 3004;
 
-// this is for the public folder on path /
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/CRMdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+// serving static files
 app.use(express.static("public"));
 
-// method to use JSON
-//app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// This is for proxies
-app.set("trust proxy", "loopback");
-
-// this is for images folder on path images
-app.use("/images", express.static("images"));
-
 app.get("/", (req, res) =>
-  // get data first
-  res.json(data)
+  res.send(`Node and express server is running on port ${PORT}`)
 );
-
-// JSON data
-// {"hello": "JSON is cool"}
-// URLEncoded data
-// hello=URLEncoded+is+cool
-
-app.post("/newItem", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
-});
-
-app.get(
-  "/item/:id",
-  (req, res, next) => {
-    // this is the middleware, that pulls the data
-    console.log(req.params.id);
-    let user = Number(req.params.id);
-    console.log(user);
-    console.log(data[user]);
-    // middleware that uses the req object
-    console.log(`Request from: ${req.originalUrl}`);
-    console.log(`Request type: ${req.method}`);
-    // everything above is middleware
-    res.send(data[user]);
-    next();
-  },
-  (req, res) => console.log("Did you get the right data?")
-);
-
-app
-  .route("/item")
-  .get((req, res) => {
-    throw new Error();
-    //res.download("images/rocket.jpg")
-    //res.redirect("http://www.google.com")
-    //res.end()
-    //res.send(`a get request with /item route on port ${PORT}`);
-  })
-  .put((req, res) => res.send(`a put request with /item route on port ${PORT}`))
-  .delete((req, res) =>
-    res.send(`a delete request with /item route on port ${PORT}`)
-  );
-
-// Error handling function
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send(`Red alert! Red alert!: ${err.stack}`);
-});
 
 app.listen(PORT, () => {
   console.log(`Your server is running on port ${PORT}`);
