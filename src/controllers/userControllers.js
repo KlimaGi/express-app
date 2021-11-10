e7f718c0468a80a0a858252cad8a555a4c438344
Giedre <giedre.klima@gmail.com>
2021-11-10 13:07:18 +0200

    login, registration handlers + security measures

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { UserSchema } from "../models/userModel";
+
+// mongoose expect values that we send from request to the API
+const User = mongoose.model("User", UserSchema);
+
+export const loginRequired = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    return res.staus(401).json({ message: "Unauthorized user!" });
+  }
+};
+
+export const register = (req, res) => {
+  const newUser = new User(req.body);
+  newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
+  newUser.save((err, user) => {
+    if (err) {
+      return res.status(400).send({ message: err });
+    } else {
+      user.hashPassword = undefined;
+      return res.json(user);
+    }
+  });
+};
